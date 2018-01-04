235c91945009023e8adbc372caa2ae9d68b51e8f
Aaron Klein <aklein@riotgames.com>
2018-01-03 18:40:09 -0800

    Added the ability to toggle between videos

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetails from './components/video_details';
 
 YTSearch({key: process.env.YT_APIKEY, term: 'surfboards'}, function(data) {
   console.log(data);
@@ -14,9 +15,15 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { "videos" : [] };
+    this.state = { 
+      "videos" : [], 
+      "selectedVideo" : null 
+    };
     YTSearch({key: process.env.YT_APIKEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos });
+      this.setState({ 
+        videos: videos,
+        selectedVideo: videos[0] 
+      });
     });
   }
 
@@ -25,7 +32,11 @@ class App extends Component {
       <div>
         <SearchBar />
         {/* example of passing props */}
-        <VideoList videos={this.state.videos} />
+        <VideoDetails video={this.state.selectedVideo} />
+        <VideoList 
+          onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
+          videos={ this.state.videos } 
+          />
       </div>
     );
   }
