0e250c7ca6f6c62c4f8680b28c0719e40363014d
Aaron Klein <aklein@riotgames.com>
2018-01-03 17:53:28 -0800

    added list of videos and list view for items

---
+++ src/components/video_list.js
@@ -0,0 +1,18 @@
+//no state, so functional component is fine
+import React from 'react';
+
+import VideoListItem from './video_list_item';
+
+const VideoList = (props) => {
+    const videoItems = props.videos.map((video) => { 
+        return <VideoListItem key={video.etag} video={video} />
+    });
+
+    return (
+        <ul className="col-md-4 list-group">
+            {videoItems}
+        </ul>
+    )
+}
+
+export default VideoList;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 YTSearch({key: process.env.YT_APIKEY, term: 'surfboards'}, function(data) {
   console.log(data);
 });
 
 // create a component
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { "videos" : [] };
+    YTSearch({key: process.env.YT_APIKEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        {/* example of passing props */}
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 // take a component and put it on the page
